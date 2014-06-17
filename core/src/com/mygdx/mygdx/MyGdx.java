4ff3e5a2ab2b569bcce4dd7487e84d692eb76754
jenny <yk494@cornell.edu>
2014-06-17 17:09:37 +0900

    assets, custom model

--- core/src/com/mygdx/mygdx/MyGdx.java
+++ core/src/com/mygdx/mygdx/MyGdx.java
@@ -3,6 +3,8 @@ package com.mygdx.mygdx;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.assets.loaders.ModelLoader;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.PerspectiveCamera;
@@ -16,8 +18,10 @@ import com.badlogic.gdx.graphics.g3d.ModelBatch;
 import com.badlogic.gdx.graphics.g3d.ModelInstance;
 import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
 import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
+import com.badlogic.gdx.graphics.g3d.loader.ObjLoader;
 import com.badlogic.gdx.graphics.g3d.utils.CameraInputController;
 import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
+import com.badlogic.gdx.utils.Array;
 
 public class MyGdx implements ApplicationListener {
 
@@ -26,45 +30,57 @@ public class MyGdx implements ApplicationListener {
 	public PerspectiveCamera cam;
 	public ModelBatch modelBatch;
 	public Model model;
-	public ModelInstance instance;
+	public AssetManager assets;
+	public Array<ModelInstance> instances = new Array<ModelInstance>();
+	public boolean loading;
 
 	@Override
 	public void create() {
+		
+		modelBatch = new ModelBatch();
+
 		environment = new Environment();
 		environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f,
 				0.4f, 0.4f, 1f));
 		environment.add(new DirectionalLight().set(0.8f, 0.8f, 0.8f, -1f,
 				-0.8f, -0.2f));
 		
-		modelBatch = new ModelBatch();
-
 		cam = new PerspectiveCamera(67, Gdx.graphics.getWidth(),
 				Gdx.graphics.getHeight());
-		cam.position.set(10f, 10f, 10f);
+		cam.position.set(1f, 1f, 1f);
 		cam.lookAt(0, 0, 0);
 		cam.near = 1f;
 		cam.far = 300f;
 		cam.update();
-
-		ModelBuilder modelBuilder = new ModelBuilder();
-		model = modelBuilder.createBox(5f, 5f, 5f,
-				new Material(ColorAttribute.createDiffuse(Color.GREEN)),
-				Usage.Position | Usage.Normal);
-		instance = new ModelInstance(model);
 		
 		camController = new CameraInputController(cam);
 		Gdx.input.setInputProcessor(camController);
+		
+		assets = new AssetManager();
+		assets.load("data/ship.obj", Model.class);
+		loading = true;
+		
+
+	}
+	
+	private void doneLoading(){
+		Model ship = assets.get("data/ship.obj", Model.class);
+		ModelInstance shipInstance = new ModelInstance(ship);
+		instances.add(shipInstance);
+		loading = false;
 	}
 
 	@Override
 	public void render() {
+		if (loading && assets.update())
+			doneLoading();
 		camController.update();
 		Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(),
 				Gdx.graphics.getHeight());
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
 
 		modelBatch.begin(cam);
-		modelBatch.render(instance, environment);
+		modelBatch.render(instances, environment);
 		modelBatch.end();
 
 	}
@@ -84,6 +100,7 @@ public class MyGdx implements ApplicationListener {
 	@Override
 	public void dispose() {
 		modelBatch.dispose();
-		model.dispose();
+		instances.clear();
+		assets.dispose();
 	}
 }
